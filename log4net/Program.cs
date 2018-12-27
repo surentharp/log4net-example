45bbbe9186759a092998eac55803c43347e2a654
Surenthar P <surenthar.p@caliberinterconnect.net>
2018-12-27 10:01:16 +0530

    source code update

---
+++ log4net/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace log4net
+{
+    // add [assembly: log4net.Config.XmlConfigurator(Watch=true)] in the AssemblyInfo.cs file under the Properties folder
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            //If you use the below commented line, it will output in the console window
+            //log4net.Config.BasicConfigurator.Configure();
+            //log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
+
+            log.Info("test");
+            Console.WriteLine("Finished");
+            Console.ReadLine();
+        }
+    }
+}
